2f42910ce68e179fc9d2c01b2015acd070c85f0f
Kamilly <kamilly.santos.05@edu.unipar.br>
2025-12-08 23:39:44 -0300

    atualizacoes

--- src/main/java/com/sop/model/Tarefa.java
+++ src/main/java/com/sop/model/Tarefa.java
@@ -1,8 +1,11 @@
 package com.sop.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalTime;
 
 @Entity
 @Getter
@@ -14,6 +17,9 @@ public class Tarefa {
 
     private String titulo;
     private String descricao;
+
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime horario;
 
     @ManyToOne
     @JoinColumn(name = "usuario_id")
